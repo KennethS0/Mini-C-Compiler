edf2d21ae22bb8498456a87a7ed0582c95d6aa7a
Jose <mikomeka1@gmail.com>
2021-04-21 17:16:06 -0600

    Added Test-Files

---
+++ testing-files/Test-Variables.c
@@ -0,0 +1,17 @@
+int main() {
+    int 1intIncorrecto;
+
+    int intCorrecto = 234;
+    float floatCorrecto = -234.543;
+    float 2floatCorrecto = 234.45;
+
+    float flotanteConExponente = 234E5;
+    float flotanteConExponente2 = 234e5;
+    float flotanteConExponente3 = 234.5E-5;
+
+    int numeroOctal = 010
+
+    int numeroHexadecimal = 0xAB
+
+    char caracterIncorrecto = 'hola'
+}
