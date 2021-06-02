556624d3d318def1e8ab23923f314a771ea8ea60
Kenneth Sánchez <38081826+KennethS0@users.noreply.github.com>
2021-06-02 15:26:43 -0600

    Switch & Const a como dijo doña teacher

--- testing-files/ScannerTest.c
+++ testing-files/ScannerTest.c
@@ -2,10 +2,6 @@ int liñsttest[1];
 int xd;
 
 void lamaooo(int xd, long oaksdoka12akmsd) {
-    xd++;
-
-    a = 13
-
-    b += 1931*(1281021839191398198398-(1%10));
-
+    const int reeee = 12;
+    
 }
