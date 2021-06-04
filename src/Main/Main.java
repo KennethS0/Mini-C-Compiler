256c9258c67aa6c140f820e2c225e6a09489c0ec
Kenneth Sánchez <38081826+KennethS0@users.noreply.github.com>
2021-06-04 16:12:11 -0600

    args in main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,25 +3,25 @@ package Main;
 import java.io.File;
 import java.io.StringReader;
 import java.nio.file.Files;
-import java.sql.SQLOutput;
-
 import Parser.Parser;
 import Scanner.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        File archivo2;
+        for (int i = 0; i < args.length; i++) {
+            File archivo;
 
-        archivo2 = new File("testing-files/ScannerTest.c");
+            archivo = new File(args[i]);
 
-        String ST = new String(Files.readAllBytes(archivo2.toPath()));
-        Scanner scanner = new Scanner(new StringReader(ST));
-        Parser s = new Parser(scanner);
-        s.parse();
+            String ST = new String(Files.readAllBytes(archivo.toPath()));
+            Scanner scanner = new Scanner(new StringReader(ST));
+            Parser s = new Parser(scanner);
+            s.parse();
 
-        //System.out.println(scanner.toString());
-        System.out.println(s.toString());
+            //System.out.println(scanner.toString());
+            System.out.println(s.toString());
+        }
         /*
         for(String argument: args){
             System.out.println("Analysis File: " + argument);
