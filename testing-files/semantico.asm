ec94d842983ea78d22758a5555a46046f0a285bc
Jose <mikomeka1@gmail.com>
2021-06-28 18:19:54 -0600

    Added check to quant and type of fun params

--- testing-files/semantico.asm
+++ testing-files/semantico.asm
@@ -1,17 +1,5 @@
 funcion1D:
-	While_Label0:
-		mov ax, 2
-		mov bx, 1
-		cmp ax, bx
-		jne Exit_Label0
-jmp While_Label0 ;CONTINUE
-	While_Label1:
-		mov ax, 3
-		mov bx, 1
-		cmp ax, bx
-		jne Exit_Label1
-jmp Exit_Label1 ;BREAK
-		jmp While_Label1
-	Exit_Label1:
-		jmp While_Label0
-	Exit_Label0:
+	mov ax, [jajaINT]
+	inc ax
+	mov [jajaINT], ax
+
--- testing-files/semantico.c
+++ testing-files/semantico.c
@@ -1,11 +1,5 @@
-void funcion1D(int jaja) {
 
-    while(2 == 1) {
-        continue;
-
-        while( 3 == 1){
-            break;
-        }
+void funcion1D(int jajaINT, char jCHAR, int mINT) {
+ funcion1D(29, jCHAR, 45);
+    jajaINT++;
     }
-
-}
